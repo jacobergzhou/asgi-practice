347981e3a3acaa92282a380e5c5032d9ba80e7fe
Yinsheng Zhou <jacobzhou98@gmail.com>
2025-09-07 13:23:28 -0700

    Initial commit for toy asgi app implementation

---
+++ raw_asgi.py
@@ -0,0 +1,23 @@
+async def app(scope, receive, send): 
+    assert scope['type'] == 'http'
+
+    body = b''
+    while True:
+        message = await receive()
+        if message['type'] == 'http.request':
+            body += message.get('body', b'')
+            if not message.get('more_body', False): 
+                break
+        elif message['type'] == 'http.disconnect':
+            break
+
+    await send({
+        'type': 'http.response.start',
+        'status': 200,
+        'headers': [[b'content-type', b'text/plain']]
+    })
+
+    await send({
+        'type': 'http.response.body',
+        'body': b'Hello ASGI!'
+    })
