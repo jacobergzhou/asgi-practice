3caee18f3215c714d7ed11a892ba497a4940d2eb
Yinsheng Zhou <jacobzhou98@gmail.com>
2025-09-07 14:59:50 -0700

    Added query parameter parsing

--- asgi_deep_dive.py
+++ asgi_deep_dive.py
@@ -7,19 +7,40 @@ async def app(scope, receive, send):
     path = scope['path']
     query_string = scope['query_string'].decode('utf-8')
 
+    params = parse_query_params(query_string)
+
     body = await read_body(receive)
 
     ### Route handling
     if path == '/' and method == 'GET':
         await send_response(send, 200, "GET Request Succeed!")
     elif path == '/users' and method == 'GET':
+        page = params.get('page', '1')
+        limit = params.get('limit', '10')
         await send_json(send, 200, {
-            'users': ['Saka', 'Rice', 'Saliba']
+            'users': ['Saka', 'Rice', 'Saliba'],
+            'page': page,
+            'limit': limit
         })
     elif path == '/echo' and method == 'POST':
         await send_response(send, 200, f'You sent: {body.decode()}')
     else:
         await send_response(send, 404, f'Path {path} not found!')
+
+def parse_query_params(query_string): 
+    if not query_string:
+        return {}
+    
+    params = {}
+    pairs = query_string.split('&')
+    for pair in pairs:
+        if '=' in pair:
+            key, value = pair.split('=')
+            params[key] = value
+        else:
+            # when key have no value: ?debug&verbose
+            params[pair] = ''
+    return params
 
 async def read_body(receive):
     body = b''
